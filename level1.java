7622f1f86211765926cb41773ed775096aa4d9e1
Florian Gruber <161wit07@o365.htl-leoben.at>
2018-04-11 13:41:40 +0200

    level1 angelegt

---
+++ level1.java
@@ -0,0 +1,13 @@
+package ccc_vendingmachine_gruber;
+
+public class level1 {
+
+	public static void main(String[] args) {
+		int eingeworfen,change;
+		String input = "12 12 23";
+		String[] splitted = new String[input.length()];
+		
+		splitted = input.split(" ");
+
+	}
+}

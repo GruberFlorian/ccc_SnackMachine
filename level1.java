a274da40a14bbef6eebfd4f4fbaa2776ec806ed6
Florian Gruber <161wit07@o365.htl-leoben.at>
2018-04-11 14:21:38 +0200

    level1 und 2

--- level1.java
+++ level1.java
@@ -3,11 +3,33 @@ package ccc_vendingmachine_gruber;
 public class level1 {
 
 	public static void main(String[] args) {
-		int eingeworfen,change;
-		String input = "12 12 23";
-		String[] splitted = new String[input.length()];
+		int betrag = Integer.parseInt(args[0]);
+		int anzahlMuenzen = Integer.parseInt(args[1]);
+		int[] muenzen = new int [anzahlMuenzen];
+		int eingeworfen = 0;
 		
-		splitted = input.split(" ");
+		for(int m = 0; m<anzahlMuenzen;m++) {
+			int offset = 2;
+			muenzen[m] = Integer.parseInt(args[offset + m]);
+		}
+		
+		for (int i = 0; i<anzahlMuenzen;i++) {
+			eingeworfen = muenzen[i] + eingeworfen; 
+		}
+		
+		if(betrag - eingeworfen < 0) {
+			System.out.println("CHANGE:" + (betrag-eingeworfen)*-1);
+		}
+		else if (betrag -eingeworfen == 0) {
+			
+			System.out.println("genau");
+		}
+		else {
+			System.out.println("MISSING:" + (betrag -eingeworfen));
+		}
+			
+		System.out.println(eingeworfen);
+		
 
 	}
 }
---
+++ level2.java
@@ -0,0 +1,10 @@
+package ccc_vendingmachine_gruber;
+
+public class level2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

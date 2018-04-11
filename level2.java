f0416860bd77b69e6335cff4915d5ee882d53e8e
Florian Gruber <161wit07@o365.htl-leoben.at>
2018-04-11 15:34:32 +0200

    level2.java

--- level2.java
+++ level2.java
@@ -23,6 +23,7 @@ public class level2 {
 
 
 		if(betrag - eingeworfen < 0) {
+			int zwischen = 0;
 			euro[0] = 1;
 			euro[1] = 2;
 			euro[2] = 5;
@@ -32,16 +33,57 @@ public class level2 {
 			euro[6] = 100;
 			euro[7] = 200;
 			rueckgabe = (betrag-eingeworfen)*-1;
-			
-			
-			
-			
-			
+
+
+
+			if(rueckgabe % euro[7] >0 ) {
+
+				ausgabemuenzen[7] = rueckgabe/ euro[7];
+				rueckgabe = rueckgabe%euro[7];
+			}
+			if(rueckgabe % euro[6] >0 ) {
+
+				ausgabemuenzen[6] = rueckgabe / euro[6];
+				rueckgabe = rueckgabe%euro[6];
+			}
+			if(rueckgabe % euro[5] >0 ) {
+
+				ausgabemuenzen[5] = rueckgabe / euro[5];
+				rueckgabe = rueckgabe%euro[5];
+			}
+			if(rueckgabe % euro[4] >0 ) {
+
+				ausgabemuenzen[4] =rueckgabe / euro[4];
+				rueckgabe = rueckgabe%euro[4];
+			}
+			if(rueckgabe % euro[3] >0 ) {
+
+				ausgabemuenzen[3] =rueckgabe / euro[3];
+				rueckgabe = rueckgabe%euro[3];
+			}
+			if(rueckgabe % euro[2] >0 ) {
+
+				ausgabemuenzen[2] =rueckgabe/ euro[2];
+				rueckgabe = rueckgabe%euro[2];
+			}
+			if(rueckgabe % euro[1] >0 ) {
+
+				ausgabemuenzen[1] = rueckgabe / euro[1];
+				rueckgabe = rueckgabe%euro[1];
+			}
+			if(rueckgabe % euro[0] ==0 ) {
+
+				ausgabemuenzen[0] =rueckgabe / euro[0];
+				rueckgabe = rueckgabe%euro[0];
+			}
+
+
+
 			System.out.print("CHANGE:");
 			for(int z = 0; z<8;z++) {
-				System.out.println(ausgabemuenzen[z]);
+				System.out.print(ausgabemuenzen[z]);
 			}
-			
+
 		}
 		else if (betrag -eingeworfen == 0) {
 
@@ -51,8 +93,8 @@ public class level2 {
 			System.out.println("MISSING:" + (betrag -eingeworfen));
 		}
 
-		System.out.println(rueckgabe % euro[7]);
-		
+
+
 
 
 	}
